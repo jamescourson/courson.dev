ee8819ce4c5f857c4b9f771d692df58e9b59d9ed
James Courson <brokencontigo@gmail.com>
2025-02-20 15:03:13 -0500

    remove day-night toggle

--- index.js
+++ index.js
@@ -13,15 +13,21 @@ async function getHTMLElementFromTemplateFile(filePath) {
 class SharedHeaderElement extends HTMLElement {
   connectedCallback() {
     const shadowRoot = this.attachShadow({ mode: 'open' });
-    getHTMLElementFromTemplateFile('./templates/header.html')
-      .then(headerElement => shadowRoot.appendChild(headerElement.content));
+    getHTMLElementFromTemplateFile('./templates/header/header.html')
+      .then(headerElement => {
+        // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template#avoiding_documentfragment_pitfalls
+
+        // Clone template with listener, add outer listener, inject
+        const headerElementClone = headerElement.content.cloneNode(true);
+        shadowRoot.appendChild(headerElementClone);
+    });
   }
 }
 
 class SharedFooterElement extends HTMLElement {
   connectedCallback() {
     const shadowRoot = this.attachShadow({ mode: 'open' });
-    getHTMLElementFromTemplateFile('./templates/footer.html')
+    getHTMLElementFromTemplateFile('./templates/footer/footer.html')
       .then(footerElement => shadowRoot.appendChild(footerElement.content));
   }
 }
