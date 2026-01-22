5d290981ec183b5e4e396a64ae2816d4b7e0a865
James Courson <brokencontigo@gmail.com>
2026-01-22 14:55:28 -0500

    add resources, implement render, update version

--- src/pages/resources/Resources.jsx
+++ src/pages/resources/Resources.jsx
@@ -1,19 +1,38 @@
+import { useEffect, useState } from 'react';
+
+import { default as resourcesJson } from '~/data/resources.json';
+
 import './Resources.scss';
 
 
-const Resources = () => (
-  <main id="page-resources">
-    <hgroup>
-      <h1>Resources</h1>
-      <em>Some development resources and tools that I use often.</em>
-      <hr className="hr-partial" />
-    </hgroup>
+const Resources = () => {
+  const [resourceList, setResourceList] = useState([]);
 
-    <div>
-      <b>TODO: actually add some resources</b>
-    </div>
-  </main>
-);
+  useEffect(() => {
+    if (resourceList.length === 0)
+      setResourceList(resourcesJson);
+  }, []);
+
+  return (
+    <main id="page-resources">
+      <hgroup>
+        <h1>Resources</h1>
+        <em>Some development resources and tools that I use often.</em>
+        <hr className="hr-partial" />
+      </hgroup>
+
+      <ul>
+        {resourceList.map(({ title, url, description }, i) => (
+          <li key={i}>
+            <h3>{title}</h3>
+            <em>{description}</em> <br />
+            <small><a href={url}>Check it out</a></small>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
 
 export default Resources;
 
