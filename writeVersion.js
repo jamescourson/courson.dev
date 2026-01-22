dd541e0de8fc5edd1b84f65378414ca4889737f7
James Courson <brokencontigo@gmail.com>
2026-01-22 11:08:28 -0500

    add comments

--- writeVersion.js
+++ writeVersion.js
@@ -1,11 +1,16 @@
 import { readFileSync, writeFileSync } from "fs";
 import { resolve } from "path";
 
+
+// get version number
 const outputPath = resolve(process.cwd(), "public/version.txt");
 const packageObj = JSON.parse(
   readFileSync(resolve(process.cwd(), "package.json"), "utf8"));
 const version = packageObj.version;
 
+// write to file
 writeFileSync(outputPath, `${version}\n`);
+
+// confirm write during build
 console.log(`${version} ➔ public/version.txt`);
 
